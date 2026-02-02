7dc8c91635d0c824bb925eafbd79f81bd67848f0
Faridh Mendoza <faridh.mendoza@descartesmauss.ai>
2026-02-02 18:08:46 +0100

    Introduces errors in preparation for ruff

--- database/models/public_models.py
+++ database/models/public_models.py
@@ -1,6 +1,9 @@
 from datetime import datetime
-from enum import Enum
+from enum import Enum, IntEnum
+from typing import List, Optional
+from uuid import UUID, uuid4
 
+from sqlalchemy import JSON, Column
 from sqlmodel import Field, SQLModel
 
 
@@ -71,30 +74,30 @@ class Client(SQLModel, table=True):
     __tablename__ = "cs_interface_client"
     __table_args__ = {"schema": "public"}
 
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     company: str = Field(default="D&M")
     customer_id: str
     last_updated: datetime = Field(default_factory=datetime.utcnow)
     hidden: bool = False
     # industry: Optional[Industry] = Field(default=None)
-    industry: str | None = Field(default=None)
-    tier_id: int | None = Field(default=None, foreign_key="service_tier.id")
-    website: str | None = None
-    company_profile_image_s3_uri: str | None = None
+    industry: Optional[str] = Field(default=None)
+    tier_id: Optional[int] = Field(default=None, foreign_key="service_tier.id")
+    website: Optional[str] = None
+    company_profile_image_s3_uri: Optional[str] = None
 
 
 class Sow(SQLModel, table=True):
     __tablename__ = "cs_interface_sow"
     __table_args__ = {"schema": "public"}
 
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     client_id: int = Field(foreign_key="client.id")
     name: str
-    description: str | None = None
-    sow_id: str | None = None
+    description: Optional[str] = None
+    sow_id: Optional[str] = None
     sow_status: str = Field(default="disabled")
     last_updated: datetime = Field(default_factory=datetime.utcnow)
     hidden: bool = False
-    feed_group_id: int | None = None
-    geography_id: str | None = Field(default=None, foreign_key="geography.geography_id")
-    tier_id: int | None = Field(default=None, foreign_key="service_tier.id")
+    feed_group_id: Optional[int] = None
+    geography_id: Optional[str] = Field(default=None, foreign_key="geography.geography_id")
+    tier_id: Optional[int] = Field(default=None, foreign_key="service_tier.id")
