b83b76b604e4cddb012d876b512bbde573e12fa6
Faridh Mendoza <faridh.mendoza@descartesmauss.ai>
2026-02-02 14:28:34 +0100

    Testing pre-commit

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from fastapi.responses import JSONResponse
 
 from database.manager import get_all
 from database.models.public_models import Client
-from database.models.tenant_models import SOW
+
 from jwt_validator import validate_jwt
+
+from database.models.tenant_models import SOW
 
 app = FastAPI()
 
